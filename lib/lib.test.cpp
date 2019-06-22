b84429692e22b6ae9526aec0d2fa288bb970ff60
XzoRit <xzorit@hotmail.com>
2019-06-22 11:58:36 +0200

    add test for join algorithm

--- lib/lib.test.cpp
+++ lib/lib.test.cpp
@@ -1,8 +1,23 @@
-#include <lib/lib.hpp>
 #include <boost/test/unit_test.hpp>
+#include <range/v3/algorithm/equal.hpp>
+#include <range/v3/range/access.hpp>
+#include <range/v3/view/join.hpp>
+#include <vector>
 
-BOOST_AUTO_TEST_CASE(lib_add)
+BOOST_AUTO_TEST_CASE(range_join)
 {
-  const auto a{ xzr::lib::add(1, 3) };
-  BOOST_TEST(a != 4);
+  using namespace ranges;
+
+  const std::vector<std::vector<int>> ii{ { 1 }, { 2, 3 }, { 4, 5, 6 } };
+  const auto& flatii{ view::join(ii) };
+  BOOST_TEST(ranges::equal(flatii, (std::vector<int>{ 1, 2, 3, 4, 5, 6 })));
+
+  const std::vector<std::vector<std::vector<int>>> iii{
+    { { 1 }, { 2, 3 }, { 4, 5, 6 } }, { { 7, 8 }, { 9, 10 } }, { { 11, 12 } }
+  };
+  const auto& flatiii{ iii | view::join | view::join };
+  BOOST_TEST(ranges::equal(
+    flatiii, (std::vector<int>{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 })));
+
+  BOOST_TEST(*ranges::cbegin(flatiii) == 1);
 }
